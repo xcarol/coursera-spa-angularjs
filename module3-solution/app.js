fe0e93948f27f55e21502efb0745c12ed2f264a6
Xavier Carol <xcr@aquilessolutions.com>
2016-10-03 10:37:42 +0100

    done

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,14 +3,42 @@
 
   angular.module('NarrowItDownApp', [])
          .controller('NarrowItDownController', NarrowItDownController)
-         .service('MenuSearchService', MenuSearchService);
+         .service('MenuSearchService', MenuSearchService)
+         .directive('foundItems', FoundItemsDirective);
+
+  function FoundItemsDirective() {
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      scope: {
+        items: '<',
+        onRemove: '&'
+      },
+      controllerAs: 'ctrl',
+      bindToController: true
+    };
+
+    return ddo;
+  }
 
   NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 
   function NarrowItDownController($scope, MenuSearchService) {
     var ctrl = this;
-    ctrl.narrowSearch = function(search) {
-      ctrl.found = MenuSearchService.getMatchedMenuItems();
+
+    ctrl.search_term = "";
+
+    ctrl.narrowSearch = function() {
+      if (ctrl.search_term.length == 0) {
+        ctrl.items = [];
+      } else {
+        MenuSearchService.getMatchedMenuItems(ctrl.search_term).then(function(foundItems){
+          ctrl.items = foundItems;
+        });
+      }
+    }
+
+    ctrl.removeItem = function(index) {
+      ctrl.items.splice(index,1);
     }
   }
 
@@ -24,12 +52,15 @@
             method: 'GET',
             url: 'https://davids-restaurant.herokuapp.com/menu_items.json'})
           .then(function (result) {
-            // process result and only keep items that match
-            var foundItems = result.data;
+            var foundItems = [];
+            var menu_items = result.data.menu_items;
+            var search_regex = new RegExp(searchTerm, "i");
 
-            console.log(result.data);
+            for(var i = 0 ; i < menu_items.length ; i++) {
+              if (menu_items[i].description.search(search_regex) != -1)
+                foundItems.push(menu_items[i]);
+            }
 
-            // return processed items
             return foundItems;
       });
     }

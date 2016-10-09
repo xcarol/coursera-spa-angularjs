1793a876fc1279f6185b96f50c6f770ebb7b47a3
xcarol <xcarolr@gmail.com>
2016-10-09 21:27:05 +0200

    more

--- module4-solution/routes.js
+++ module4-solution/routes.js
@@ -5,18 +5,18 @@
     .config(RoutesConfig);
 
     RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-    
+
     function RoutesConfig($stateProvider, $urlRouterProvider) {
 
     $urlRouterProvider.otherwise('/');
-    
+
     $stateProvider
-    
+
     .state('home', {
         url: '/',
         template:'<a ui-sref="categories">See our Premade No Cookie Shopping List!</a>'
     })
-    
+
     .state('categories', {
         url: '/categories',
         templateUrl: './main-categories.template.html',
@@ -27,20 +27,20 @@
             }]
         }
     })
-    
+
     .state('categories.itemDetail', {
-        templateUrl: './items.template.html',
+        templateUrl: './items-template.html',
         controller: 'CategoryDetailController as itemDetail',
         params: {
             itemId: null
-        },
+        }/*,
         resolve:{
             itemDetail: ['MenuDataService', function (MenuDataService, params) {
                 return MenuDataService.getItemsForCategory(params.itemId);
             }]
-        }
+        }*/
     });
-    
+
     }
 
 })();
